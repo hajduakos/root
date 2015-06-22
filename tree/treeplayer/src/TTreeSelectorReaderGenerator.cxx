70ac5b5c513267f483086c3404f5487ac56825fe
Akos Hajdu <akos.hajdu@cern.ch>
2015-06-22 14:57:35 +0200

    Looping through branches.

--- tree/treeplayer/src/TTreeSelectorReaderGenerator.cxx
+++ tree/treeplayer/src/TTreeSelectorReaderGenerator.cxx
@@ -12,13 +12,40 @@
 #include "TTreeSelectorReaderGenerator.h"
 #include <stdio.h>
 
+#include "TTree.h"
+
 namespace ROOT {
 
    TTreeSelectorReaderGenerator::TTreeSelectorReaderGenerator(TTree* tree,
-                                            const char *classname, UInt_t maxUnrolling)
+                                            const char *classname, UInt_t maxUnrolling) : 
+      fTree(tree),
+      fClassname(classname),
+      fMaxUnrolling(maxUnrolling)
    {
       // Constructor.
-
-      printf("TTreeSelectorReaderGenerator::ctor\n");
+      AnalyzeTree(fTree);
+      
+      WriteSelector();
+   }
+   
+   void TTreeSelectorReaderGenerator::AnalyzeTree(TTree *tree)
+   {
+      // Analyze tree
+      TIter next(tree->GetListOfBranches());
+      TBranch *branch;
+      
+      // Loop through branches
+      while (branch = (TBranch*)next()) {
+         const char *branchname = branch->GetName();
+         const char *classname = branch->GetClassName();
+         
+         printf("Branch name: %s, class name: %s\n", branch->GetName(), branch->GetClassName());
+         
+      }
+   }
+   
+   void TTreeSelectorReaderGenerator::WriteSelector()
+   {
+      // Generate code for selector class
    }
 }
